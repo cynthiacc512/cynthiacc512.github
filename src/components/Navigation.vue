cc94fb8dbe1bc2672eaa69ff6d61e8518d9a4109
youkwhd <youkwhd@gmail.com>
2021-10-04 01:10:33 +0700

    (fix): dynamically render data and fix navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,35 @@
 <template>
-  <nav>
-    <ul id="navigation">
-      <li><a class="active" id="scrollToAboutMe">About Me</a></li>
-      <li><a id="scrollToWorkExperience">Work Experience</a></li>
-      <li><a id="scrollToEducation">Education</a></li>
-      <li><a id="scrollToSkills">Skills</a></li>
-      <li><a id="scrollToContactMe">Contact Me</a></li>
-    </ul>
-  </nav>
+  <div>
+    <nav>
+      <ul id="navigation" v-bind:class="isActive && `active`">
+        <li><a href="#about-me">About Me</a></li>
+        <li><a href="#work-experience">Work Experience</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact-me">Contact Me</a></li>
+      </ul>
+    </nav>
+    <div @click="toggleMenu()" v-bind:class="isActive ? `menu-btn disabled` : `menu-btn`">
+      <div class="menu-btn__burger" id="burger1"></div>
+      <div class="menu-btn__burger" id="burger2"></div>
+      <div class="menu-btn__burger" id="burger3"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Navigation",
+  data: () => {
+    return ({
+      isActive: false,
+    })
+  },
+  methods: {
+    toggleMenu() {
+      this.isActive = !this.isActive;
+    },
+  },
 };
 </script>
 
--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -11,41 +11,14 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Frontstep</td>
-            <td>Front End Developer</td>
-            <td>Internship</td>
-            <td>Jun 2021 - Jul 2021</td>
-          </tr>
-          <tr>
-            <td>Frontstep</td>
-            <td>UI/UX Designer</td>
-            <td>Partime</td>
-            <td>May 2021 - Jul 2021</td>
-          </tr>
-          <tr>
-            <td>Magika Studio</td>
-            <td>Junior Content Designer</td>
-            <td>Partime</td>
-            <td>Agu 2020 - Apr 2021</td>
-          </tr>
-          <tr>
-            <td>Dollan</td>
-            <td>Designer Grafis dan UI/UX Designer</td>
-            <td>Freelance</td>
-            <td>Agu 2020 - Sep 2020</td>
-          </tr>
-          <tr>
-            <td>Self Employed</td>
-            <td>Freelance UI/UX Designer</td>
-            <td>Freelance</td>
-            <td>Jun 2019 - Jun 2020</td>
-          </tr>
-          <tr>
-            <td>Self Employed</td>
-            <td>Freelance Front End Web Developer</td>
-            <td>Freelance</td>
-            <td>Jun 2019 - Sep 2019</td>
+          <tr
+            v-for="(experience, index) in experiences"
+            :key="index"
+          >
+            <td>{{experience.company}}</td>
+            <td>{{experience.role}}</td>
+            <td>{{experience.status}}</td>
+            <td>{{experience.period}}</td>
           </tr>
         </tbody>
       </table>
@@ -54,6 +27,48 @@
 
 <script>
 export default {
-    name: 'WorkExperience'
+    name: 'WorkExperience',
+    data: () => {
+      return ({
+        experiences: [
+          {
+            company: 'Frontstep',
+            role: 'Front End Developer',
+            status: 'Internship',
+            period: 'Jun 2021 - Jul 2021',
+          },
+          {
+            company: 'Frontstep',
+            role: 'UI/UX Designer',
+            status: 'Partime',
+            period: 'May 2021 - Jul 2021',
+          },
+          {
+            company: 'Magika Studio',
+            role: 'Junior Content Designer',
+            status: 'Partime',
+            period: 'Agu 2020 - Apr 2021',
+          },
+          {
+            company: 'Dollan',
+            role: 'Designer Grafis dan UI/UX Designer',
+            status: 'Freelance',
+            period: 'Agu 2020 - Sep 2020',
+          },
+          {
+            company: 'Self Employed',
+            role: 'Freelance UI/UX Designer',
+            status: 'Freelance',
+            period: 'Jun 2019 - Jun 2020',
+          },
+          {
+            company: 'Self Employed',
+            role: 'Freelance Front End Web Developer',
+            status: 'Freelance',
+            period: 'Jun 2019 - Sep 2019',
+          },
+        ],
+      })
+    },
 }
 </script>
